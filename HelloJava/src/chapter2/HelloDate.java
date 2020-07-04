99363a663d60b4e46d335068cab31169e5e6ea48
william396 <hpl.jack@gmail.com>
2020-07-04 14:37:06 +0630

    fix  增加注释示例代码

--- HelloJava/src/chapter2/HelloDate.java
+++ HelloJava/src/chapter2/HelloDate.java
@@ -1,7 +1,16 @@
-package chapter2;
+package Chapter2;
 
+// Chapter2/HelloDate.java
 import java.util.Date;
 
+
+/**
+ * This is first Thinking in java example program.
+ * Display a string and today's date
+ * @author william
+ * @author www.will.net
+ * @version 1.0
+ */
 public class HelloDate {
 
     public static void main(String[] args) {
